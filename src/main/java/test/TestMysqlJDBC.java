6755131ec1db27ed24e610e962bdde33d40c5c61
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-03 20:49:42 -0500

    Conexion a JDBC

---
+++ src/main/java/test/TestMysqlJDBC.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestMysqlJDBC {
+    public static void main(String[] args) {
+        var url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        try {
+            Connection conexion = DriverManager.getConnection(url, "root", "root");
+            Statement instruccion = conexion.createStatement();
+            var sql = "SELECT * FROM test.persona";
+            ResultSet resultado = instruccion.executeQuery(sql);
+            while(resultado.next()){
+                System.out.print("Id persona: " + resultado.getInt("id_persona"));
+                System.out.print(" Nombre: " + resultado.getString("nombre"));
+                System.out.print(" Apellido: " + resultado.getString("apellido"));
+                System.out.print(" Email: " + resultado.getString("email"));
+                System.out.print(" Telefono: " + resultado.getString("telefono"));
+                System.out.println("");
+            }
+            resultado.close();
+            instruccion.close();
+            conexion.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        
+        
+    }
+}
