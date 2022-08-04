7a0c97f3b3c7cd45eec3137e3e09963fc4ea4bce
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-03 22:12:33 -0500

    Metodo seleccionar

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -4,18 +4,48 @@ import domain.Persona;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PersonaDAO {
+
     private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM test.persona";
-    
-    public List<Persona> seleccionar(){
+
+    public List<Persona> seleccionar() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         Persona persona = null;
         List<Persona> personas = new ArrayList<>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idPersona = rs.getInt("id_persona");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String email = rs.getString("email");
+                String telefono = rs.getString("telefono");
+
+                persona = new Persona(idPersona, nombre, apellido, email, telefono);
+
+                personas.add(persona);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(stmt);
+                Conexion.close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return personas;
     }
-    
+
 }
