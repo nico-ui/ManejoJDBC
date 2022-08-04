8372ffb8e25fa9fb6e5f8ad113a96f5795a0785e
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-08-03 21:51:14 -0500

    Patron diseño DAO en clase de dominio Persona

---
+++ src/main/java/datos/PersonaDAO.java
@@ -0,0 +1,21 @@
+package datos;
+
+import domain.Persona;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonaDAO {
+    private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM test.persona";
+    
+    public List<Persona> seleccionar(){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Persona persona = null;
+        List<Persona> personas = new ArrayList<>();
+    }
+    
+}
